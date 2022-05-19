c6343c9c5b9c97f68e95e549dc2101adf1d61a29
WinWinMaw <mawinwinmaw4@gmail.com>
2022-05-19 10:33:09 +0630

    noti

---
+++ app/Http/Controllers/SharedContactController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use App\Models\SharedContact;
+use App\Http\Requests\StoreSharedContactRequest;
+use App\Http\Requests\UpdateSharedContactRequest;
+use App\Models\User;
+use App\Notifications\ContactShareNoti;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SharedContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSharedContactRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSharedContactRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SharedContact  $sharedContact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Request $request,SharedContact $sharedContact)
+    {
+        if($sharedContact->status){
+            return abort(404);
+        }
+
+
+        $from = User::find($sharedContact->from);
+        $to = User::find($sharedContact->to);
+        $contacts = Contact::whereIn("id",json_decode($sharedContact->contact_ids))->get();
+//
+//        $mark = Auth::user()->unreadNotifications->where('id',$notificationId)->get();
+//        return $mark;
+        Auth::user()->unreadNotifications()->update(['read_at' => now()]);
+
+        return view('shared-contact.show',compact('from','to','sharedContact','contacts'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SharedContact  $sharedContact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SharedContact $sharedContact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSharedContactRequest  $request
+     * @param  \App\Models\SharedContact  $sharedContact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSharedContactRequest $request, SharedContact $sharedContact)
+    {
+        if($request->action === 'accept'){
+
+          Contact::whereIn("id",json_decode($sharedContact->contact_ids))
+                ->update(["user_id" =>Auth::id()]);
+        }
+
+        $sharedContact->status = $request->action;
+        $sharedContact->update();
+
+        return redirect()->route('contact.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SharedContact  $sharedContact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SharedContact $sharedContact)
+    {
+        //
+    }
+}
---
+++ resources/views/shared-contact/show.blade.php
@@ -0,0 +1,125 @@
+@extends("layouts.app")
+@section("content")
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <div class="text-center">
+                    Show Route
+                    <h4>Yor Got {{ $contacts->count() }} from {{ $from->email }}</h4>
+                    <p>
+                        {{ $sharedContact->message }}
+                    </p>
+
+                    <ul class="list-group">
+                        @forelse($contacts as $contact)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <div class="">
+                                    <p class="fw-bold mb-0">
+                                        {{ $contact->name }}
+                                    </p>
+                                    <p class="text-black-50 mb-0">
+                                        {{ $contact->phone }}
+                                    </p>
+                                </div>
+                            </li>
+                        @empty
+                        @endforelse
+                    </ul>
+
+                    @if(auth()->id() === $sharedContact->from)
+                        <div class="my-3">
+                            <form action="{{route('shared-contact.update',$sharedContact->id)}}" method="post">
+                                @csrf
+                                @method('put')
+                                <input type="hidden" name="action" value="cancel">
+                                <button class="btn btn-danger">
+                                    Cancel
+                                </button>
+                            </form>
+                        </div>
+                    @else
+                        <div class="my-3">
+                            <form class="d-inline" action="{{route('shared-contact.update',$sharedContact->id)}}" method="post">
+                                @csrf
+                                @method('put')
+                                <input type="hidden" name="action" value="reject">
+                                <button class="btn btn-outline-danger">
+                                    Reject
+                                </button>
+                            </form>
+                            <form class="d-inline" action="{{route('shared-contact.update',$sharedContact->id)}}" method="post">
+                                @csrf
+                                @method('put')
+                                <input type="hidden" name="action" value="accept">
+                                <button class="btn btn-primary">
+                                    Accept
+                                </button>
+                            </form>
+                        </div>
+                    @endif
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="emailModal" tabindex="-1" aria-labelledby="emailModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="emailModalLabel">Contact Share</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="">
+                        <label class="form-label" for="">Recipient Email</label>
+                        <input type="text" name="email" form="bulk_action" class="form-control">
+                    </div>
+                    <div class="">
+                        <label class="form-label" for="">Message</label>
+                        <textarea name="message" form="bulk_action" class="form-control" id="" cols="30" rows="7"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" onclick="cancelAction()"  class="btn btn-secondary">Close</button>
+                    <button type="submit" form="bulk_action" class="btn btn-primary">
+                        <i class="fa-solid fa-paper-plane"></i> Share
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @push("js")
+        <script>
+
+            let emailModal = document.querySelector("#emailModal");
+            let myEmailModal =new bootstrap.Modal(emailModal,{
+                backdrop: "static"
+            });
+
+            let contactBulkFunctionalitySelect = document.querySelector(`[name="functionality"]`);
+            contactBulkFunctionalitySelect.addEventListener("change",function (){
+                let selected = Number(this.value);
+                console.log(selected);
+
+                if(selected === 1){
+                    myEmailModal.show();
+                }
+            })
+
+            function cancelAction(){
+                contactBulkFunctionalitySelect.value = "";
+                myEmailModal.hide();
+            }
+        </script>
+    @endpush
+
+@endsection
+
+
